56fe46af6cf93a729a727f6541c801c3ad068e49
lqt0223 <lqt19920223@126.com>
2024-03-21 15:13:47 +0800

    fix

--- init.lua
+++ init.lua
@@ -850,6 +850,7 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'vue' }, -- still buggy for the vue-parser
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
