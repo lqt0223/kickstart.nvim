df87e08d0f01f422dea197a3c670e3a5651c626d
lqt0223 <lqt19920223@126.com>
2024-04-19 10:05:40 +0800

    remove unused

--- init.lua
+++ init.lua
@@ -352,7 +352,6 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-      "fdschmidt93/telescope-egrepify.nvim",
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
